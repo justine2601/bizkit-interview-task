8d27978b3431a878ddf4380bfef60d095da45151
AJCE <70326935+justine2601@users.noreply.github.com>
2023-12-20 18:50:10 +0800

    Improved Program with matching numbers and searches

--- phasebook/match.py
+++ phasebook/match.py
@@ -9,19 +9,26 @@ bp = Blueprint("match", __name__, url_prefix="/match")
 
 @bp.route("<int:match_id>")
 def match(match_id):
-    if match_id < 0 or match_id >= len(MATCHES):
+    if not 0 <= match_id < len(MATCHES):
         return "Invalid match id", 404
 
-    start = time.time()
-    msg = "Match found" if (is_match(*MATCHES[match_id])) else "No match"
-    end = time.time()
+    start_time = time.time()
+    matched_elements = find_matching_elements(*MATCHES[match_id])
+    end_time = time.time()
+# Define if there is a match number and show the matching numbers
+    if matched_elements:
+        response_data = {"Status": "Match found"}
+    else:
+        response_data = {"Status": "No match"}
 
-    return {"message": msg, "elapsedTime": end - start}, 200
+    elapsed_time = end_time - start_time
 
+    return {"response": response_data, "elapsedTime": elapsed_time}, 200
 
-def is_match(fave_numbers_1, fave_numbers_2):
-    for number in fave_numbers_2:
-        if number not in fave_numbers_1:
-            return False
+def find_matching_elements(fave_numbers_1, fave_numbers_2):
+    # Input the lists to set for efficient element operation
+    set_1 = set(fave_numbers_1)
+    set_2 = set(fave_numbers_2)
 
-    return True
+    # Return the set of matched numbers directly
+    return set_1.intersection(set_2)
--- phasebook/search.py
+++ phasebook/search.py
@@ -9,7 +9,7 @@ bp = Blueprint("search", __name__, url_prefix="/search")
 @bp.route("")
 def search():
     return search_users(request.args.to_dict()), 200
-
+    
 
 def search_users(args):
     """Search users database
@@ -22,9 +22,42 @@ def search_users(args):
             occupation: string
 
     Returns:
-        a list of users that match the search parameters
+        a list of users that match any of the search parameters
     """
+    # Initialize the results with the entire USERS list
+    results = USERS
 
-    # Implement search here!
+    # Check if any parameters are provided for ID, Name, Age, or Occupation
+    if any(param in args for param in ['id', 'name', 'age', 'occupation']):
+        user_id = args.get('id')
+        user_name = args.get('name', '').lower()
+        user_age = args.get('age')
+        user_occupation = args.get('occupation', '').lower()
 
-    return USERS
+        results = [
+            user for user in results
+            if (user_id and user['id'] == user_id) or
+               (user_name and user_name in user['name'].lower()) or
+               (user_age and int(user_age) - 1 <= user['age'] <= int(user_age) + 1) or
+               (user_occupation and user_occupation in user['occupation'].lower())
+        ]
+
+    # Deduplicate the results
+    results = list({user['id']: user for user in results}.values())
+
+    # Generate search suggestions based on available data
+    if not results:
+        suggestions = generate_search_suggestions(USERS)
+        suggestions_message = "\n".join(f"- {suggestion}" for suggestion in suggestions)
+        return f"No results found. Consider trying:\n{suggestions_message}"
+
+    return results
+
+#Generate search suggestions based on available user data
+def generate_search_suggestions(users):
+    """Generate search suggestions based on available user data"""
+    name_suggestions = set(user['name'].split()[0].lower() for user in users)
+    age_suggestions = set(str(user['age']) for user in users)
+    occupation_suggestions = set(word.lower() for user in users for word in user['occupation'].split())
+
+    return list(name_suggestions.union(age_suggestions, occupation_suggestions))
